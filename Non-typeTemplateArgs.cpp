8e52c0199c984e09ae7133f12463699924e77dbd
Siddharth Gupta <79963032+sidd12-gupta@users.noreply.github.com>
2022-08-26 00:02:19 +0530

    Add files via upload

---
+++ Non-typeTemplateArgs.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+template <class T, int size>
+class A
+{
+public:
+    T arr[size];
+
+    void insert()
+    {
+        int i = 1;
+
+        for (int j = 0; j < size; j++)
+        {
+            arr[j] = i;
+            i++;
+        }
+    }
+
+    void display()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+};
+
+int main()
+{
+    A<int, 15> b;
+    b.insert();
+    b.display();
+    return 0;
+}
